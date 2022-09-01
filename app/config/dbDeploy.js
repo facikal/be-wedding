65e3ea530253830d0d0ce2e195b0288855abcc88
facikal <fadhil1999@gmail.com>
2022-09-02 00:26:53 +0700

    fix be

--- app/config/dbDeploy.js
+++ app/config/dbDeploy.js
@@ -1,9 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize('invitation_wedding', 'xvpccpjpuokmhc', '952a533efea8f4138f855baa3f925fc70f23308f1f0b173ab4485a34ec02a092', {
-  database:"dfjnfmq1oik2l1",
-  username:"xvpccpjpuokmhc",
-  password:"952a533efea8f4138f855baa3f925fc70f23308f1f0b173ab4485a34ec02a092",
+const db = new Sequelize('dfjnfmq1oik2l1', 'xvpccpjpuokmhc', '952a533efea8f4138f855baa3f925fc70f23308f1f0b173ab4485a34ec02a092', {
   port:"5432",
   host: "ec2-52-70-45-163.compute-1.amazonaws.com",
   dialect: "postgres",
