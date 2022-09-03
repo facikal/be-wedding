2e5a2091743521ef8a6a2b0eeeff94ca7e00e530
facikal <fadhil1999@gmail.com>
2022-09-03 19:07:56 +0700

    fix be

--- app/db/seeders/20220903115010-seeder-user.js
+++ app/db/seeders/20220903115010-seeder-user.js
@@ -3,9 +3,12 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('Users', [{
+      uuid: "1235141fdds",
+      name: "cikal",
       email: "fadhil1999@gmail.com",
       password: "1234",
       confPassword: "1234",
+      role:"admin",
       createdAt: new Date(),
       updatedAt: new Date()
     }], {});
