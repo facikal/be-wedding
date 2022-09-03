6964ffd8b53407b059b149a5cceacbd23301caa5
facikal <fadhil1999@gmail.com>
2022-09-03 23:13:21 +0700

    fix be

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const app = express();
 
 app.use(cors({
   credentials: true,
-  origin: 'fe-wedding.vercel.app'
+  origin: 'https://fe-wedding.vercel.app'
 }))
 app.use(session({
   secret: process.env.SESS_SECRET,
@@ -40,6 +40,24 @@ app.use(session({
     secure: 'auto'
   }
 }))
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 app.use(FileUpload())
 app.use(logger('dev'));
 app.use(express.json());
