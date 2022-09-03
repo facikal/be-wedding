768542c5263ad7fd94a6382db69e8e0a71be9b03
facikal <fadhil1999@gmail.com>
2022-09-03 22:49:23 +0700

    fix be

--- app/config/database.js
+++ app/config/database.js
@@ -17,11 +17,14 @@ module.exports = {
     username: "xvpccpjpuokmhc",
     password: "952a533efea8f4138f855baa3f925fc70f23308f1f0b173ab4485a34ec02a092",
     database: "dfjnfmq1oik2l1",
-    port: "5432",
+    port: 5432,
     host: "ec2-52-70-45-163.compute-1.amazonaws.com",
     dialect: "postgres",
-    dialectOptions: {
-      ssl: { rejectUnauthorized: false },
-    },
+    "dialectOptions": {
+      "ssl": {
+        "require": true,
+        "rejectUnauthorized": false
+      }
+    }
   }
 }
