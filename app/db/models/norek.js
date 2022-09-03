ed1fba475fbb524569e1986d594def80e7242358
facikal <fadhil1999@gmail.com>
2022-09-03 21:24:25 +0700

    fix be

--- app/db/models/norek.js
+++ app/db/models/norek.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Norek.belongsTo(models.User)  
+      // Norek.belongsTo(models.User)  
     }
   }
   Norek.init({
