9575eb3154a388fde9dd89996ef1da96961cd7ad
facikal <fadhil1999@gmail.com>
2022-09-05 07:37:59 +0700

    fix be

--- app/config/database.js
+++ app/config/database.js
@@ -14,11 +14,11 @@ module.exports = {
     "dialect": "mysql"
   },
   production: {
-    username: "xvpccpjpuokmhc",
-    password: "952a533efea8f4138f855baa3f925fc70f23308f1f0b173ab4485a34ec02a092",
-    database: "dfjnfmq1oik2l1",
+    username: "ssyvmnsdmjinkk",
+    password: "4335bdfc508cf6ee7bb5dbd7362023b868873c42129546d916f0b5e2240d7a48",
+    database: "d4frf16vfp0a0j",
     port: 5432,
-    host: "ec2-52-70-45-163.compute-1.amazonaws.com",
+    host: "ec2-34-234-240-121.compute-1.amazonaws.com",
     dialect: "postgres",
     dialectOptions: {
       ssl: {
