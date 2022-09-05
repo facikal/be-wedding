b53e4d9e20a111d08c6b7f7158dffbe3a333cae9
facikal <fadhil1999@gmail.com>
2022-09-05 08:30:45 +0700

    fix be

--- app/config/dbDeploy.js
+++ app/config/dbDeploy.js
@@ -1,7 +1,13 @@
 const { Sequelize } = require('sequelize');
+const db = require('../db/models');
 const env = process.env.NODE_ENV || 'production';
-const config = require(__dirname + './database.js')[env];
+const config = require(__dirname + '/../../config/database.js')[env];
 
-const db = new Sequelize(new Sequelize(process.env[config.use_env_variable], config))
+let db;
+if (config.use_env_variable) {
+  db = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  db = new Sequelize(config.database, config.username, config.password, config);
+}
 
 module.exports = db
